2e1ce77b6289253f7fbc5f2dac0ae3dc3ee5e2a2
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-24 04:55:55 -0400

    Set configuration for easier access.

---
+++ a-plus-tutoring/source/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('FOLDER_PATH', __DIR__ . '/../public');
+define('SERVER_PATH', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME']);
+
+define('IMAGES_PATH', SERVER_PATH . '/core/assets/media/images');
+define('ICONS_PATH', SERVER_PATH . '/core/assets/media/icons');
