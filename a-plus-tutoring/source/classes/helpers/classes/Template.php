8d6e571a77650a40c420bf92392c034239ad62af
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-24 04:58:23 -0400

    Added template files.

---
+++ a-plus-tutoring/source/classes/helpers/classes/Template.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Source\Classes\Helpers\Classes;
+
+class Template
+{
+    public static function importViewStyleSheets(string $path)
+    {
+        $relativePath = '/core/assets/css/views/' . $path . '.css';
+
+        if (file_exists(FOLDER_PATH . $relativePath)) {
+            return '<link rel="stylesheet" href="' . SERVER_PATH . $relativePath . '">';
+        }
+
+        return '<!-- END - IMPORTED CSS STYLESHEETS -->';
+    }
+
+    public static function importViewModules(string $path) {}
+}
