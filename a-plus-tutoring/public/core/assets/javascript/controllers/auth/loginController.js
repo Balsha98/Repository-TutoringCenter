f1edfb86fff9bb0a5439a66550b16934065ad4e3
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-26 12:00:31 -0400

    Updated Login JS controller.

--- a-plus-tutoring/public/core/assets/javascript/controllers/auth/loginController.js
+++ a-plus-tutoring/public/core/assets/javascript/controllers/auth/loginController.js
@@ -1,5 +1,6 @@
 // IMPORTED MODULES
 import requestHandler from "../../requestHandler.js";
+import * as pageLoaderController from "../partials/loaders/pageLoaderController.js";
 import loginView from "../../views/auth/loginView.js";
 
 const handleUserLogin = function (e) {
@@ -16,6 +17,8 @@ const handleUserLogin = function (e) {
 };
 
 const initController = function () {
+    pageLoaderController.controlHidePageLoader(3);
+
     loginView.handleUserLogin(handleUserLogin);
 };
 
