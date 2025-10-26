7a9df0288dd5f4884e8f8df749c4ec7aaf047123
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-25 23:09:33 -0400

    Imported latest version of jQuery.

--- a-plus-tutoring/public/core/partials/footer.php
+++ a-plus-tutoring/public/core/partials/footer.php
@@ -1,5 +1,6 @@
     <!-- IMPORTED 3RD-PARTY MODULES -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script src="<?php echo SERVER_PATH . '/core/assets/libraries/jQuery.js'; ?>"></script>
     <!-- IMPORTED JS MODULES -->
 </body>
 
