98c96c3350074285fa7ec67b3322ad1c561c8957
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-25 22:51:32 -0400

    Updated configuration and autoloading.

--- a-plus-tutoring/source/autoload.php
+++ a-plus-tutoring/source/autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-$directories = require_once __DIR__ . '/classes/helpers/scripts/directories.php';
+$directories = require_once __DIR__ . '/handlers/helpers/scripts/directories.php';
 
 spl_autoload_register(function ($class) {
     global $directories;
@@ -21,7 +21,10 @@ spl_autoload_register(function ($class) {
     // Filter out class name.
     $namespaceParts = explode('\\', $class);
     $className = $namespaceParts[count($namespaceParts) - 1];
-    $classPath = __DIR__ . '/' . $classDirectory . '/' . $className . '.php';
+    $isApiClass = str_contains($class, 'Api');
+
+    // Filter out final class path.
+    $classPath = __DIR__ . '/' . ($isApiClass ? '../public/' : '') . $classDirectory . '/' . $className . '.php';
 
     if (file_exists($classPath)) {
         require_once $classPath;
--- a-plus-tutoring/source/config.php
+++ a-plus-tutoring/source/config.php
@@ -3,5 +3,9 @@
 define('FOLDER_PATH', __DIR__ . '/../public');
 define('SERVER_PATH', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME']);
 
+define('DB_NAME', 'aplus_tutoring_center');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
 define('IMAGES_PATH', SERVER_PATH . '/core/assets/media/images');
 define('ICONS_PATH', SERVER_PATH . '/core/assets/media/icons');
