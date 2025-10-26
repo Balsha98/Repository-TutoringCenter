c4e082c055ffa0019d22dc6928e8202652dadc1f
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-26 00:49:10 -0400

    Updated footer partial script.

--- a-plus-tutoring/public/core/partials/footer.php
+++ a-plus-tutoring/public/core/partials/footer.php
@@ -2,6 +2,7 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script src="<?php echo SERVER_PATH . '/core/assets/libraries/jQuery.js'; ?>"></script>
     <!-- IMPORTED JS MODULES -->
+    <?php echo Source\Handlers\Helpers\Classes\Template::importViewModules($viewData['path']); ?>
 </body>
 
 </html>
