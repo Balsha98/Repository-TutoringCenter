586ddb66280c893d2b969ba5876466f73f0027a9
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-26 12:00:42 -0400

    Updated Template helper class.

--- a-plus-tutoring/source/handlers/helpers/classes/Template.php
+++ a-plus-tutoring/source/handlers/helpers/classes/Template.php
@@ -4,7 +4,7 @@ namespace Source\Handlers\Helpers\Classes;
 
 class Template
 {
-    public static function importViewStyleSheets(string $path)
+    public static function importViewStyleSheet(string $path)
     {
         $relativePath = '/core/assets/css/views/' . $path . '.css';
 
@@ -12,10 +12,10 @@ class Template
             return '<link rel="stylesheet" href="' . SERVER_PATH . $relativePath . '">';
         }
 
-        return '<!-- END - IMPORTED CSS STYLESHEETS -->';
+        return '';
     }
 
-    public static function importViewModules(string $path)
+    public static function importViewModule(string $path)
     {
         $relativePath = '/core/assets/javascript/controllers/' . $path . 'Controller.js';
 
@@ -23,6 +23,28 @@ class Template
             return '<script type="module" src="' . SERVER_PATH . $relativePath . '"></script>';
         }
 
-        return '<!-- END - IMPORTED JS MODULES -->';
+        return '';
+    }
+
+    public static function importPartials(string $path, string $extension)
+    {
+        $dependencies = JSON::decode(file_get_contents(LOCAL_PATH . '/core/assets/dependencies/imports.json'));
+
+        $return = '';
+        foreach ($dependencies as $viewDependencies) {
+            if (isset($viewDependencies[$path])) {
+                foreach ($viewDependencies[$path][$extension] as $relativePath) {
+                    if ($extension === 'css') {
+                        $return .= '<link rel="stylesheet" href="' . SERVER_PATH . $relativePath . '.' . $extension . '">';
+
+                        continue;
+                    }
+
+                    $return .= '<script type="module" src="' . SERVER_PATH . $relativePath . '.' . $extension . '"></script>';
+                }
+            }
+        }
+
+        return $return;
     }
 }
