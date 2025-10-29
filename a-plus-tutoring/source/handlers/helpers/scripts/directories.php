aa1af838b6cc72c864695b4c497e8c8628b169b7
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-29 07:46:19 -0400

    Added new autoload directories.

--- a-plus-tutoring/source/handlers/helpers/scripts/directories.php
+++ a-plus-tutoring/source/handlers/helpers/scripts/directories.php
@@ -3,6 +3,7 @@
 return [
     'Api\Assets\Routing',
     'Api\Assets\Controllers',
+    'Api\Assets\Controllers\Auth',
     'Handlers\Core\Database',
     'Handlers\Core\Models',
     'Handlers\Core\Routing',
