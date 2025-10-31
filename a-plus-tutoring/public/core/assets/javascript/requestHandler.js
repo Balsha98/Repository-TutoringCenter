31cc9e097a0faa06aa457fab47f971d097a14656
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-31 01:21:55 -0400

    Updated requestHandler JS script.

--- a-plus-tutoring/public/core/assets/javascript/requestHandler.js
+++ a-plus-tutoring/public/core/assets/javascript/requestHandler.js
@@ -2,29 +2,29 @@
 import * as alertModalController from "./controllers/partials/modals/alertModalController.js";
 
 class RequestHandler {
-    handleRequest(url, method, data) {
-        $.ajax({
+    async handleRequest(url, method, data) {
+        const response = await $.ajax({
             url: url,
             method: method,
             data: JSON.stringify(data),
-            success: function (response) {
-                alertModalController.controlShowAlertModal(response);
-            },
         });
+
+        if (response["input-id"]) this.#highlightInvalidInput(response);
+
+        alertModalController.controlShowAlertModal(response);
     }
 
-    checkForEmptyInputs(data) {
-        const validations = [];
-
+    traverseForInvalidInputs(data) {
         for (const [key, value] of Object.entries(data)) {
-            if (!value) {
-                $(`#${key}`).closest("div").addClass("div-invalid-input-container");
-
-                validations.push(key);
-            }
+            if (!value) $(`#${key}`).closest("div").addClass("div-invalid-input-container");
         }
 
-        return validations.length;
+        return $(".div-invalid-input-container").length;
+    }
+
+    #highlightInvalidInput(response) {
+        const parent = $(`#${response["input-id"]}`).closest("div");
+        $(parent).addClass("div-invalid-input-container");
     }
 }
 
