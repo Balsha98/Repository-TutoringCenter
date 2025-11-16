619905bda2527ae13096c0147e3c21a3e873cf39
Balša Bazović <balsa.bazovic@gmail.com>
2025-11-16 16:13:33 -0500

    Updated Tutor Profile view.

--- a-plus-tutoring/public/core/views/profile/tutor.php
+++ a-plus-tutoring/public/core/views/profile/tutor.php
@@ -37,7 +37,73 @@
                 </ul>
             </nav>
         </header>
+        <?php $status = $tutor->getStatus(); ?>
         <div class="div-tutor-profile-view-overview-container">
-            <!-- TODO: Here we go... -->
+            <div class="div-tutor-profile-view-brief-bio-container">
+                <?php $icon = $status === 'active' ? 'check' : 'x'; ?>
+                <div class="div-tutor-profile-view-brief-bio-image-container">
+                    <img src="<?php echo $tutor->getImage(); ?>" alt="Tutor Profile Image">
+                    <div class="div-tutor-profile-view-brief-bio-status-container <?php echo $status; ?>">
+                        <ion-icon src="<?php echo ICONS_PATH . '/' . $icon . '.svg'; ?>"></ion-icon>
+                    </div>
+                </div>
+                <div class="div-tutor-profile-view-brief-bio-data-container">
+                    <p><?php echo $tutor->getFirstName(); ?> <?php echo $tutor->getLastName(); ?></p>
+                    <span><?php echo $tutor->getEmail(); ?></span>
+                </div>
+            </div>
+            <form class="form-tutor-profile-view-detailed-bio" action="/api/profile/tutor" method="PUT">
+                <div class="div-multi-input-containers grid-2-columns">
+                    <div class="div-input-container">
+                        <input id="first_name" type="text" name="first_name" value="<?php echo $tutor->getFirstName(); ?>" placeholder="First Name">
+                        <label for="first_name">
+                            <ion-icon src="<?php echo ICONS_PATH . '/user.svg'; ?>"></ion-icon>
+                        </label>
+                    </div>
+                    <div class="div-input-container">
+                        <input id="last_name" type="text" name="last_name" value="<?php echo $tutor->getLastName(); ?>" placeholder="Last Name">
+                        <label for="last_name">
+                            <ion-icon src="<?php echo ICONS_PATH . '/user.svg'; ?>"></ion-icon>
+                        </label>
+                    </div>
+                </div>
+                <div class="div-multi-input-containers grid-2-columns">
+                    <div class="div-input-container">
+                        <input id="email_address" type="email" name="email_address" value="<?php echo $tutor->getEmail(); ?>" placeholder="Email Address">
+                        <label for="email_address">
+                            <ion-icon src="<?php echo ICONS_PATH . '/mail.svg'; ?>"></ion-icon>
+                        </label>
+                    </div>
+                    <div class="div-input-container">
+                        <input id="password" type="password" name="password" placeholder="Password">
+                        <label for="password">
+                            <ion-icon src="<?php echo ICONS_PATH . '/lock.svg'; ?>"></ion-icon>
+                        </label>
+                    </div>
+                </div>
+                <div class="div-multi-input-containers grid-2-columns">
+                    <div class="div-input-container">
+                        <input id="phone" type="text" name="phone" value="<?php echo $tutor->getPhone(); ?>" placeholder="Phone Number">
+                        <label for="phone">
+                            <ion-icon src="<?php echo ICONS_PATH . '/phone.svg'; ?>"></ion-icon>
+                        </label>
+                    </div>
+                    <div class="div-input-container">
+                        <select id="status" name="status" value="<?php echo $status; ?>">
+                            <option value="">Status</option>
+                            <option value="inactive" <?php echo $status === 'inactive' ? 'selected' : ''; ?>>Inactive</option>
+                            <option value="active" <?php echo $status === 'active' ? 'selected' : ''; ?>>Active</option>
+                        </select>
+                        <label for="status">
+                            <ion-icon src="<?php echo ICONS_PATH . '/activity.svg'; ?>"></ion-icon>
+                        </label>
+                    </div>
+                </div>
+            </form>
         </div>
+        <?php $dateObj = date_create($tutor->getDateHired()); ?>
+        <footer class="footer-tutor-profile-view-container">
+            <p>Member since <?php echo $dateObj->format('j F, Y'); ?>.</p>
+            <button class="btn btn-primary btn-profile-update">Update</button>
+        </footer>
     </div>
