0309d73b525028c171800aa64c7a1bda40968bd2
Balša Bazović <balsa.bazovic@gmail.com>
2025-11-17 17:59:45 -0500

    Updated Student partial scripts.

--- a-plus-tutoring/public/core/views/dashboard/student.php
+++ a-plus-tutoring/public/core/views/dashboard/student.php
@@ -4,56 +4,12 @@
     <!-- STUDENT DASHBOARD VIEW CONTAINER -->
     <div class="div-student-dashboard-view-container">
         <?php $dbInstance = Source\Handlers\Helpers\Classes\Session::getDb(); ?>
-        <?php $student = new Source\Handlers\Core\Models\Student($dbInstance); ?>
-        <header class="header-student-dashboard-view-container">
-            <p>Welcome, <span><?php echo $student->getFirstName(); ?></span>!</p>
-            <nav class="nav-student-dashboard-view-container">
-                <ul class="nav-student-dashboard-view-container-list">
-                    <li class="nav-student-dashboard-view-container-list-item active">
-                        <a class="link link-icon" href="/dashboard">
-                            <ion-icon src="<?php echo ICONS_PATH . '/grid.svg'; ?>"></ion-icon>
-                        </a>
-                    </li>
-                    <li class="nav-student-dashboard-view-container-list-item">
-                        <a class="link link-icon" href="/sessions">
-                            <ion-icon src="<?php echo ICONS_PATH . '/calendar.svg'; ?>"></ion-icon>
-                        </a>
-                    </li>
-                    <li class="nav-student-dashboard-view-container-list-item">
-                        <a class="link link-icon" href="/profile/<?php echo $student->getID(); ?>">
-                            <ion-icon src="<?php echo ICONS_PATH . '/user.svg'; ?>"></ion-icon>
-                        </a>
-                    </li>
-                    <li class="nav-student-dashboard-view-container-list-item">
-                        <button class="btn btn-icon btn-logout-modal-toggle">
-                            <ion-icon src="<?php echo ICONS_PATH . '/log-out.svg'; ?>"></ion-icon>
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <?php $user = new Source\Handlers\Core\Models\Student($dbInstance); ?>
+        <?php require_once LOCAL_PATH . '/core/partials/nav.php'; ?>
         <div class="div-student-dashboard-view-overview-container grid-2-columns">
             <div class="div-student-dashboard-view-overview-brand">
                 <img src="<?php echo IMAGES_PATH . '/site-logo-inverted-traced.png'; ?>" alt="Inverted Website Logo Traced">
             </div>
-            <div class="div-student-dashboard-view-overview-controls">
-                <p>What would you like to <span>view</span> today?</p>
-                <nav class="nav-student-dashboard-view-overview-controls">
-                    <ul class="nav-student-dashboard-view-overview-controls-list">
-                        <li class="nav-student-dashboard-view-overview-controls-list-item">
-                            <a class="link link-primary" href="/sessions">
-                                <span>Sessions</span>
-                                <ion-icon src="<?php echo ICONS_PATH . '/chevron-right.svg'; ?>"></ion-icon>
-                            </a>
-                        </li>
-                        <li class="nav-student-dashboard-view-overview-controls-list-item">
-                            <a class="link link-primary" href="/profile/<?php echo $student->getID(); ?>">
-                                <span>Profile</span>
-                                <ion-icon src="<?php echo ICONS_PATH . '/chevron-right.svg'; ?>"></ion-icon>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            <?php require_once LOCAL_PATH . '/core/partials/views/dashboard/controls.php'; ?>
         </div>
     </div>
--- a-plus-tutoring/public/core/views/profile/student.php
+++ a-plus-tutoring/public/core/views/profile/student.php
@@ -1,3 +1,4 @@
+    <?php require_once LOCAL_PATH . '/core/partials/modals/alert-modal.php'; ?>
     <?php require_once LOCAL_PATH . '/core/partials/loaders/page-loader.php'; ?>
     <?php require_once LOCAL_PATH . '/core/partials/modals/logout-modal.php'; ?>
     <?php require_once LOCAL_PATH . '/core/partials/modals/password-modal.php'; ?>
@@ -6,57 +7,31 @@
     <!-- STUDENT PROFILE VIEW CONTAINER -->
     <div class="div-student-profile-view-container">
         <?php $dbInstance = Source\Handlers\Helpers\Classes\Session::getDb(); ?>
-        <?php $student = new Source\Handlers\Core\Models\Student($dbInstance); ?>
-        <header class="header-student-profile-view-container">
-            <p>Welcome, <span><?php echo $student->getFirstName(); ?></span>!</p>
-            <nav class="nav-student-profile-view-container">
-                <ul class="nav-student-profile-view-container-list">
-                    <li class="nav-student-profile-view-container-list-item">
-                        <a class="link link-icon" href="/dashboard">
-                            <ion-icon src="<?php echo ICONS_PATH . '/grid.svg'; ?>"></ion-icon>
-                        </a>
-                    </li>
-                    <li class="nav-student-profile-view-container-list-item">
-                        <a class="link link-icon" href="/sessions">
-                            <ion-icon src="<?php echo ICONS_PATH . '/calendar.svg'; ?>"></ion-icon>
-                        </a>
-                    </li>
-                    <li class="nav-student-profile-view-container-list-item active">
-                        <a class="link link-icon" href="/profile/<?php echo $student->getID(); ?>">
-                            <ion-icon src="<?php echo ICONS_PATH . '/user.svg'; ?>"></ion-icon>
-                        </a>
-                    </li>
-                    <li class="nav-student-profile-view-container-list-item">
-                        <button class="btn btn-icon btn-logout-modal-toggle">
-                            <ion-icon src="<?php echo ICONS_PATH . '/log-out.svg'; ?>"></ion-icon>
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <?php $user = new Source\Handlers\Core\Models\Student($dbInstance); ?>
+        <?php require_once LOCAL_PATH . '/core/partials/nav.php'; ?>
         <div class="div-student-profile-view-overview-container">
             <div class="div-student-profile-view-brief-bio-container">
                 <div class="div-student-profile-view-brief-bio-image-data-container">
                     <button class="btn btn-icon btn-primary btn-image-modal-toggle">
                         <ion-icon src="<?php echo ICONS_PATH . '/upload.svg'; ?>"></ion-icon>
                     </button>
-                    <img src="<?php echo $student->getImage(); ?>" alt="Student Profile Image">
+                    <img src="<?php echo $user->getImage(); ?>" alt="Student Profile Image">
                 </div>
                 <div class="div-student-profile-view-brief-bio-data-container">
-                    <p><?php echo $student->getFirstName(); ?> <?php echo $student->getLastName(); ?></p>
-                    <span><?php echo $student->getEmail(); ?></span>
+                    <p><?php echo $user->getFirstName(); ?> <?php echo $user->getLastName(); ?></p>
+                    <span><?php echo $user->getEmail(); ?></span>
                 </div>
             </div>
             <form class="form-student-profile-view-detailed-bio" action="/api/profile/student" method="PUT">
                 <div class="div-multi-input-containers grid-2-columns">
                     <div class="div-input-container">
-                        <input id="first_name" type="text" name="first_name" value="<?php echo $student->getFirstName(); ?>" placeholder="First Name">
+                        <input id="first_name" type="text" name="first_name" value="<?php echo $user->getFirstName(); ?>" placeholder="First Name">
                         <label for="first_name">
                             <ion-icon src="<?php echo ICONS_PATH . '/user.svg'; ?>"></ion-icon>
                         </label>
                     </div>
                     <div class="div-input-container">
-                        <input id="last_name" type="text" name="last_name" value="<?php echo $student->getLastName(); ?>" placeholder="Last Name">
+                        <input id="last_name" type="text" name="last_name" value="<?php echo $user->getLastName(); ?>" placeholder="Last Name">
                         <label for="last_name">
                             <ion-icon src="<?php echo ICONS_PATH . '/user.svg'; ?>"></ion-icon>
                         </label>
@@ -64,13 +39,13 @@
                 </div>
                 <div class="div-multi-input-containers grid-2-columns">
                     <div class="div-input-container">
-                        <input id="email_address" type="email" name="email_address" value="<?php echo $student->getEmail(); ?>" placeholder="Email Address">
+                        <input id="email_address" type="email" name="email_address" value="<?php echo $user->getEmail(); ?>" placeholder="Email Address">
                         <label for="email_address">
                             <ion-icon src="<?php echo ICONS_PATH . '/mail.svg'; ?>"></ion-icon>
                         </label>
                     </div>
                     <div class="div-input-container">
-                        <input id="phone" type="text" name="phone" value="<?php echo $student->getPhone(); ?>" placeholder="Phone Number">
+                        <input id="phone" type="text" name="phone" value="<?php echo $user->getPhone(); ?>" placeholder="Phone Number">
                         <label for="phone">
                             <ion-icon src="<?php echo ICONS_PATH . '/phone.svg'; ?>"></ion-icon>
                         </label>
@@ -78,21 +53,23 @@
                 </div>
                 <div class="div-multi-input-containers grid-2-columns">
                     <div class="div-input-container">
-                        <input id="grade" type="text" name="grade" value="<?php echo $student->getGradeLabel(); ?>" placeholder="Grade Level">
+                        <input id="grade" type="text" name="grade" value="<?php echo $user->getGradeLabel(); ?>" placeholder="Grade Level">
                         <label for="grade">
                             <ion-icon src="<?php echo ICONS_PATH . '/activity.svg'; ?>"></ion-icon>
                         </label>
                     </div>
                     <div class="div-input-container">
-                        <input id="major" type="text" name="major" value="<?php echo $student->getMajor(); ?>" placeholder="Major">
+                        <input id="major" type="text" name="major" value="<?php echo $user->getMajor(); ?>" placeholder="Major">
                         <label for="major">
                             <ion-icon src="<?php echo ICONS_PATH . '/book-open.svg'; ?>"></ion-icon>
                         </label>
                     </div>
                 </div>
+                <input id="id" type="hidden" value="<?php echo $user->getID(); ?>">
+                <input id="role" type="hidden" value="<?php echo $user->getRole(); ?>">
             </form>
         </div>
-        <?php $dateObj = date_create($student->getDateEnrolled()); ?>
+        <?php $dateObj = date_create($user->getDateEnrolled()); ?>
         <footer class="footer-student-profile-view-container">
             <p>Enrolled on <?php echo $dateObj->format('j F, Y'); ?>.</p>
             <div class="div-footer-student-profile-view-form-controls-container">
