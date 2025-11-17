ec1304509af73a0756ad6bae232dbbf617bcc473
Balša Bazović <balsa.bazovic@gmail.com>
2025-11-17 17:57:01 -0500

    Updated Image Modal partial scripts.

--- a-plus-tutoring/public/core/partials/modals/image-modal.php
+++ a-plus-tutoring/public/core/partials/modals/image-modal.php
@@ -9,14 +9,14 @@
                     <h2>Upload Your Profile Image URL</h2>
                     <p>Please provide your image <span>url</span> in the following field.</p>
                 </header>
-                <form class="form-image-modal" action="/api/profile/image/tutor" method="PUT">
+                <form class="form-image-modal" action="/api/profile/image" method="PUT">
                     <div class="div-input-container">
                         <input id="image" type="text" name="image" placeholder="Image URL">
                         <label for="image">
                             <ion-icon src="<?php echo ICONS_PATH . '/image.svg'; ?>"></ion-icon>
                         </label>
                     </div>
-                    <button class="btn btn-primary btn-image-update">
+                    <button class="btn btn-primary btn-image-upload">
                         <ion-icon src="<?php echo ICONS_PATH . '/upload.svg' ?>"></ion-icon>
                         <span>Upload</span>
                     </button>
