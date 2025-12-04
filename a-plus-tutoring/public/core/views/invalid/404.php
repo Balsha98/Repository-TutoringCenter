4f96cdb013d9eaf7950c363f6cb8b8f80f87949f
Balša Bazović <balsa.bazovic@gmail.com>
2025-12-03 22:08:03 -0500

    Updated 404 view.

---
+++ a-plus-tutoring/public/core/views/invalid/404.php
@@ -0,0 +1,13 @@
+    <!-- 404 VIEW CONTAINER -->
+    <div class="div-404-view-container">
+        <h1>Oops!</h1>
+        <div class="div-404-view-content-container">
+            <header class="header-404-view-content-container">
+                <h2>Requested Page Was Not Found</h2>
+                <p>The page you are looking for unfortunately does <span>not</span> exist.</p>
+            </header>
+            <a class="link link-primary" href="/dashboard">
+                <span>Dashboard</span>
+            </a>
+        </div>
+    </div>
